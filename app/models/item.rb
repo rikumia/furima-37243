c14e8b052a7d370d9f647ff2b1589dab7fd52303
rikumia <nori_1234@icloud.com>
2021-12-09 12:40:43 +0900

    商品出品機能

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+  belongs_to  :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :pay_for_shipping
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_explanation, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :product_condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :pay_for_shipping_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+end
---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    item_name             {"aaa"}
+    item_explanation      {"aaa"}
+    category_id           {"2"}
+    product_condition_id  {"2"}
+    pay_for_shipping_id   {"2"}
+    shipping_area_id      {"2"}
+    days_to_ship_id       {"2"}
+    price                 {"3000"}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
